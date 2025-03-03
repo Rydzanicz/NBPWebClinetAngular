094e47d7d5cacbfa282b9f6a2744e2adc1340175
Rydzanicz <asddsfsdfsf@gmail.com>
2025-03-03 09:36:17 +0100

    Added sorting functionality and improved empty data handling in the table component.

--- NBPWebClinetAngular/src/app/table-container/table-container.component.ts
+++ NBPWebClinetAngular/src/app/table-container/table-container.component.ts
@@ -1,14 +1,39 @@
-import {Component, Input} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {CommonModule} from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-table-container',
   templateUrl: './table-container.component.html',
   styleUrls: ['./table-container.component.css'],
   imports: [FormsModule, CommonModule],
-
 })
 export class TableContainerComponent {
   @Input() data: any[] = [];
+
+  sortState: { column: string, direction: 'ASC' | 'DESC' } = { column: '', direction: 'ASC' }; // Stan sortowania
+
+  sortData(column: 'exchangeRateDateForBuyAndSell' | 'buy' | 'sell'): void {
+    if (!this.data || this.data.length === 0) return;
+
+    if (this.sortState.column !== column) {
+      this.sortState.direction = 'ASC';
+    }
+
+    this.data.sort((a, b) => {
+      const valueA = column === 'exchangeRateDateForBuyAndSell'
+        ? new Date(a[column]).getTime()
+        : a[column];
+      const valueB = column === 'exchangeRateDateForBuyAndSell'
+        ? new Date(b[column]).getTime()
+        : b[column];
+
+      return this.sortState.direction === 'ASC' ? valueA - valueB : valueB - valueA;
+    });
+
+    this.sortState = {
+      column,
+      direction: this.sortState.direction === 'ASC' ? 'DESC' : 'ASC',
+    };
+  }
 }
